dca2c60a4b771479ad907d85cf426f132f7bbd6b
Jonathan Loy <jonrloy@gmail.com>
2018-05-28 17:47:23 -0400

    Adding medal rewards for score

--- slappy_bird/states/ScoreState.lua
+++ slappy_bird/states/ScoreState.lua
@@ -10,6 +10,9 @@
 
 ScoreState = Class{__includes = BaseState}
 
+local GOLD_MEDAL_IMAGE = love.graphics.newImage('images/gold_medal.png')
+local SILVER_MEDAL_IMAGE = love.graphics.newImage('images/silver_medal.png')
+local BRONZE_MEDAL_IMAGE = love.graphics.newImage('images/bronze_medal.png')
 --[[
     When we enter the score state, we expect to receive the score
     from the play state so we know what to render to the State.
@@ -32,6 +35,17 @@ function ScoreState:render()
 
     love.graphics.setFont(mediumFont)
     love.graphics.printf('Score: ' .. tostring(self.score), 0, 100, VIRTUAL_WIDTH, 'center')
-
+    if self.score >= 10 then 
+        love.graphics.printf('You\'re amazing!', 0, 140, VIRTUAL_WIDTH, 'center')
+        love.graphics.draw(GOLD_MEDAL_IMAGE, VIRTUAL_WIDTH / 2 + 100,  VIRTUAL_HEIGHT /2 - 25)
+    elseif self.score >= 5 then
+        love.graphics.printf('You\'re pretty good!', 0, 140, VIRTUAL_WIDTH, 'center')
+        love.graphics.draw(SILVER_MEDAL_IMAGE, VIRTUAL_WIDTH / 2 + 100,  VIRTUAL_HEIGHT /2 - 40)
+    elseif self.score >= 1 then
+        love.graphics.printf('At least you tried.', 0, 140, VIRTUAL_WIDTH, 'center')
+        love.graphics.draw(BRONZE_MEDAL_IMAGE, VIRTUAL_WIDTH / 2 + 100,  VIRTUAL_HEIGHT / 2 - 40 )
+    else
+        love.graphics.printf('Better Luck Next Time !', 0, 140, VIRTUAL_WIDTH, 'center')
+    end
     love.graphics.printf('Press Enter to Play Again!', 0, 160, VIRTUAL_WIDTH, 'center')
 end
